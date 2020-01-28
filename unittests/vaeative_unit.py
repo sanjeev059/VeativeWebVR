e12a831b98eb4001586ba8a5e032031af1599f5d
Sanjeev Ravindranath Desai <srdesai@radisys.com>
2020-01-28 19:37:02 +0530

    added impact analysis test cases in low

--- unittests/vaeative_unit.py
+++ unittests/vaeative_unit.py
@@ -502,25 +502,6 @@ class Test(unittest.TestCase):
         self.driver.get('http://ec2-52-5-117-32.compute-1.amazonaws.com/WebVR/Aframe/ms100057/')
         self.driver.implicitly_wait(20)
         return 0
-    
-    
-    def test_impact_analysis_activity_info(self):
-        self.driver = webdriver.Chrome(executable_path="C:\chromedriver_win32\chromedriver.exe")
-        self.driver.get('http://ec2-52-5-117-32.compute-1.amazonaws.com/unicef/public/report')
-        self.driver.implicitly_wait(10)
-        loginurl = '//a[contains(text(),\'Login\')]'
-        username = '//input[@id=\'unicef_username\']'
-        passwd = '//input[@id=\'unicef_password\']'
-        loginbtn = '//input[@id=\'login-btn\']'
-        clicable = '//div[contains(text(),\'In phenol, hydroxy functional group is directly at\')]'     
-        loginelement = WebDriverWait(self.driver,10).until(lambda driver:self.driver.find_element_by_xpath(loginurl))
-        loginelement.click()
-        user_element = WebDriverWait(self.driver,10).until(lambda driver:self.driver.find_element_by_xpath(username))
-        user_element.send_keys("sanjeev")
-        passwd_element = WebDriverWait(self.driver,10).until(lambda driver:self.driver.find_element_by_xpath(passwd))
-        passwd_element.send_keys("admin1234")
-        btn_element = WebDriverWait(self.driver,10).until(lambda driver:self.driver.find_element_by_xpath(loginbtn))
-        btn_element.click()
         
 
         self.driver = webdriver.Chrome(executable_path="C:\chromedriver_win32\chromedriver.exe")
